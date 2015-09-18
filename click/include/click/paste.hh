6e8a52db8c923b69fdc589ffbc78ac600d2d47f5
Chang Lan <clan@eecs.berkeley.edu>
2015-09-17 17:02:10 -0700

    click and dpdk impl

---
+++ click/include/click/paste.hh
@@ -0,0 +1,23 @@
+//I always import the same stuff, so put it all here
+//so we don't forget the obvious stuff.
+//
+#ifndef PASTE_H
+#define PASTE_H
+
+#define DEBUG 1
+#define MAX_TOKEN_SIZE 32 
+#define MIN_TOKEN_SIZE 8
+
+#define KEYSFILE "bb.keys"
+
+#define SERVER "169.229.49.137"
+//#define SERVER "127.0.0.1"
+//##define SERVER "10.0.8.37"
+#define PORT 3333
+
+#include <cstring>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#endif
